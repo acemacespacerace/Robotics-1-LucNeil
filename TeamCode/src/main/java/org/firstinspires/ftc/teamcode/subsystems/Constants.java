915293a139e8dbea639b2118ff4f56d64fcba1a7
neilviradia <nviradia30@gmail.com>
2026-03-27 17:13:04 -0400

    >Added all files from old, reset gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Constants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+
+public class Constants {
+    public final double speedMultiplier = 1;
+    public final double submerseExt = 200; // TODO: change
+    public final double basketExt = 200; // TODO: change
+    public final double specimenExt = 200; // TODO: change
+    public final double tuckedExt = 0; // TODO: change
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class DriveSubsystem {
+    //    These lines are used to declare our motors & imu
+    public DcMotor frontLeftMotor;
+    public DcMotor backLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor backRightMotor;
+    public IMU imu;
+    public ElapsedTime time = new ElapsedTime();
+    Telemetry tele;
+    Constants constants;
+    public DriveSubsystem(HardwareMap hardwareMap, Telemetry telemetry){
+//        These initialization lines are passed through when we transfer it into the TeleOp
+        frontLeftMotor = hardwareMap.dcMotor.get("fl");
+        backLeftMotor = hardwareMap.dcMotor.get("bl");
+        frontRightMotor = hardwareMap.dcMotor.get("fr");
+        backRightMotor = hardwareMap.dcMotor.get("br");
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+        imu.initialize(parameters);
+
+        tele = telemetry;
+    } // initialization
+
+    public void FieldCentric(double y, double x, double rx){
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower * constants.speedMultiplier);
+        backLeftMotor.setPower(backLeftPower * constants.speedMultiplier);
+        frontRightMotor.setPower(frontRightPower * constants.speedMultiplier);
+        backRightMotor.setPower(backRightPower * constants.speedMultiplier);
+    } // field centric code
+
+    public void RobotCentric(double y, double cx, double rx){
+        double x = cx * 1.1;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower * constants.speedMultiplier);
+        backLeftMotor.setPower(backLeftPower * constants.speedMultiplier);
+        frontRightMotor.setPower(frontRightPower * constants.speedMultiplier);
+        backRightMotor.setPower(backRightPower * constants.speedMultiplier);
+    } // robot centric code
+
+    public void Forward(long duration, double power){
+        time.reset();
+        while (time.seconds() <= duration) {
+            this.frontLeftMotor.setPower(power);
+            this.backLeftMotor.setPower(power);
+            this.frontRightMotor.setPower(power);
+            this.backRightMotor.setPower(power);
+        } if (time.seconds() > duration){
+            this.frontLeftMotor.setPower(0);
+            this.backLeftMotor.setPower(0);
+            this.frontRightMotor.setPower(0);
+            this.backRightMotor.setPower(0);
+        }
+    }
+
+    public void Backward(long duration, double power){
+        time.reset();
+        while (time.seconds() <= duration) {
+            this.frontLeftMotor.setPower(-power);
+            this.backLeftMotor.setPower(-power);
+            this.frontRightMotor.setPower(-power);
+            this.backRightMotor.setPower(-power);
+        } if (time.seconds() > duration){
+            this.frontLeftMotor.setPower(0);
+            this.backLeftMotor.setPower(0);
+            this.frontRightMotor.setPower(0);
+            this.backRightMotor.setPower(0);
+        }
+    }
+
+    public void Right(long duration, double power){
+        time.reset();
+        while (time.seconds() <= duration) {
+            this.frontLeftMotor.setPower(power);
+            this.backLeftMotor.setPower(-power);
+            this.frontRightMotor.setPower(-power);
+            this.backRightMotor.setPower(power);
+        } if (time.seconds() > duration){
+            this.frontLeftMotor.setPower(0);
+            this.backLeftMotor.setPower(0);
+            this.frontRightMotor.setPower(0);
+            this.backRightMotor.setPower(0);
+        }
+    }
+
+    public void Left(long duration, double power){
+        time.reset();
+        while (time.seconds() <= duration) {
+            this.frontLeftMotor.setPower(-power);
+            this.backLeftMotor.setPower(power);
+            this.frontRightMotor.setPower(power);
+            this.backRightMotor.setPower(-power);
+        } if (time.seconds() > duration){
+            this.frontLeftMotor.setPower(0);
+            this.backLeftMotor.setPower(0);
+            this.frontRightMotor.setPower(0);
+            this.backRightMotor.setPower(0);
+        }
+    }
+}
