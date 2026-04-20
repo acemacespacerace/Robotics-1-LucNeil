7db1bf029039473480ae90b3b031055391be5c93
student <nviradia30A@gmail.com>
2026-04-20 12:40:35 -0400

    >handsSubsystem ourgh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Constants.java
@@ -1,11 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 public class Constants {
     public final double speedMultiplier = 1;
-//    public final int submerseAng = 2000; // TODO: change
-//    public final int basketAng = 200; // TODO: change
     public final int specimenAng = 3500; // TODO: change
-//    public final int submerseExt = 13000; // TODO: change
-//    public final int basketExt = 2000; // TODO: change
     public final int specimenExt = 13100; // TODO: change
     public final int tucked = 10;
+    public final double sampleDistance = 20;
 }
